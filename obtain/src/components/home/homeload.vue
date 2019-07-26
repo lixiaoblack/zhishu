d79f8af00080fee67dc5efc32abe63eef4cf456f
cs <email@example.com>
2019-07-26 21:14:26 +0800

    cs66

---
+++ obtain/src/components/home/homeload.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <img src="../../../static/home.jpg" height="100%" width="100%">
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
